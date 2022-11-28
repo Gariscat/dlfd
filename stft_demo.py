e9279c7639694ec11d958ecb74e91b63f6af7ab4
Gariscat <xl3133@nyu.edu>
2022-11-28 18:57:01 +0800

    attempt_plot_stft

---
+++ stft_demo.py
@@ -0,0 +1,19 @@
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+
+from utils import *
+
+for i in (1, 3, 5, 7, 9):
+    train_data, test_data = get_data(
+        trace_path='./traces/trace.log',
+        source_id=i,
+        obs_ord=2,
+        scale=1e8,
+    )
+    sig = test_data[:10000, 0]
+    fft = librosa.stft(sig)
+    mag, pha = np.abs(fft), np.angle(fft)
+    plt.imshow(mag)
+    plt.savefig(f'./temp_node{i}.jpg')
+    plt.close()
